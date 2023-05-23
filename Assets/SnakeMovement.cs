795b702dd679c7b30cae0da216749a3cf6d4c257
BlazingFlame15 <bl8ze4@gmail.com>
2023-05-22 20:57:32 -0400

    Movement

--- Assets/SnakeMovement.cs
+++ Assets/SnakeMovement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class SnakeMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    [SerializeField] float speed = 1f;
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float rawHInput = Input.GetAxisRaw("Horizontal");
+        float rawVInput = Input.GetAxisRaw("Vertical");
+
+        if(horizontalInput == 0 && verticalInput == 0)
+        {
+            horizontalInput = 1;
+            verticalInput = 0;
+        }
+        else if(rawHInput != 0 && rawVInput == 0)
+        {
+            horizontalInput = rawHInput;
+            verticalInput = 0;
+        }
+        else if(rawHInput == 0 && rawVInput != 0)
+        {
+            horizontalInput = 0;
+            verticalInput = rawVInput;
+        }
+        else if(rawHInput != 0 && rawVInput != 0)
+        {
+            horizontalInput = rawHInput;
+            verticalInput = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {   
+        transform.position = transform.position + new Vector3(horizontalInput * speed, verticalInput * speed, 0);
     }
 }
